9f198fe0f0c9a03d878ba4a498200399d306f0fd
Jamie Bort <jamiebort@gmail.com>
2021-10-29 10:48:13 -0400

    Complete first assignment.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 const todoList = [
   {
-    uniqueIdentifier:1,
+    uniqueIdentifier: 1,
     title: "The Bad Guys in Attack of the Zittens",
   },
   {
-    uniqueIdentifier:2,
+    uniqueIdentifier: 2,
     title: "Knuffle Bunny : a cautionary tale",
   },
   {
-    uniqueIdentifier:3,
+    uniqueIdentifier: 3,
     title: "Far from home",
   },
 ];
@@ -21,18 +21,6 @@ function App() {
       <h1>Todo List</h1>
       <ul>{todoList.map((item) => <li key={item.uniqueIdentifier}>{item.title}</li>)}</ul>
     </div>
-
-    // <div style={{ textAlign: 'center' }}>
-    //   <header>
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-
   );
 }
 
