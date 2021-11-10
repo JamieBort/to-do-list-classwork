da54061570adbeb58949565e979d5b35c7b637a3
Jamie Bort <jamiebort@gmail.com>
2021-11-10 10:27:53 -0500

    Modify the src/App.js file.

--- src/App.js
+++ src/App.js
@@ -3,22 +3,21 @@ import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
 function App() {
-  // const [newTodo, setNewTodo] = useState();
-  const [todoList, setTodoList] = useState([]);
+	// const [newTodo, setNewTodo] = useState();
+	const [ todoList, setTodoList ] = useState([]);
 
-  const addTodo = (newTodo) => {
-    // Call the setTodoList state setter and use the spread operator to pass the existing Objects in the todoList Array along with the newTodo Object
-    setTodoList([...todoList, newTodo]);
-  }
+	const addTodo = (newTodo) => {
+		// Call the setTodoList state setter and use the spread operator to pass the existing Objects in the todoList Array along with the newTodo Object
+		setTodoList([ ...todoList, newTodo ]);
+	};
 
-  return (
-    <div>
-      <h1>Todo List</h1>
-      <AddTodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} />
-
-    </div>
-  );
+	return (
+		<div>
+			<h1>Todo List</h1>
+			<AddTodoForm onAddTodo={addTodo} />
+			<TodoList todoList={todoList} />
+		</div>
+	);
 }
 
 export default App;
