cbb5d3ffa8f280b3d07b29eaeef58dc307ea8118
Jamie Bort <jamiebort@gmail.com>
2022-01-12 10:07:51 -0500

    Make slight changes to break it and observe.

--- src/App.js
+++ src/App.js
@@ -4,44 +4,28 @@ import React, { useState, useEffect } from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
-// const useSemiPersistentState = () => {
-// 	const [ todoList, setTodoList ] = useState([]);
-// 	// This saves the list in local storage (in the browser).
-// 	useEffect(
-// 		() => {
-// 			if (todoList.length > 0) {
-// 				console.log('todoList is not empty', todoList);
-// 				localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-// 			} else console.log('todoList is empty', todoList);
-// 		},
-// 		[ todoList ],
-// 	);
-
-// 	// Now your list is saved in Local Storage, but when you refresh the page it disappears.
-// 	// This is because we wrote the list data to Local Storage but we aren't reading it when the application is rendered. Let's fix that:
-// 	useEffect(() => {
-// 		if (localStorage.getItem('savedTodoList') === null)
-// 			console.log('local storage is empty: ', localStorage.getItem('savedTodoList'));
-// 		else {
-// 			console.log('local storage is not empty: ', localStorage.getItem('savedTodoList'));
-// 			setTodoList(JSON.parse(localStorage.getItem('savedTodoList')));
-// 		}
-// 	}, []);
-// 	// Add a return statement in useSemiPersistentState that returns the todoList state variable and setter in an Array (just like how it's returned from the useState hook)
-// 	return [ todoList, setTodoList ];
-// };
-
 function App() {
 	// const [ todoList, setTodoList ] = useSemiPersistentState();
 	const [ todoList, setTodoList ] = useState([]);
 
-	useEffect(() => {
-		new Promise((resolve, reject) => {
-			setTimeout(() => {
-				resolve({ data: { todoList: todoList } });
-			}, 2000);
-		});
-	}, []);
+	// useEffect(() => {
+	// 	new Promise((resolve, reject) => {
+	// 		setTimeout(() => {
+	// 			console.log('done');
+	// 			// resolve({
+	// 			// 	data: {
+	// 			// 		todoList: todoList,
+	// 			// 	},
+	// 			// });
+	// 			// resolve({
+	// 			// 	data: {
+	// 			// 		todoList: JSON.parse(localStorage.getItem('savedTodoList')),
+	// 			// 	},
+	// 			// });
+	// 		}, 2000);
+	// 	});
+	// 	// .then((result)=>{setTodoList(result.data.todoList)});
+	// }, []);
 
 	// This saves the list in local storage (in the browser).
 	useEffect(
